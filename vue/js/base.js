a12063cc0ea87933b894a66411d5a67d730dfe42
chenzejiang <>
2020-03-13 18:31:38 +0800

    新增获取页面参数

--- vue/js/base.js
+++ vue/js/base.js
@@ -8,6 +8,16 @@
         cn: '我很努力了',
         fastAudio: ''
     },
+    created() {
+        function getQueryString(name) { 
+            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); 
+            var r = window.location.search.substr(1).match(reg); 
+            if (r != null) return decodeURIComponent(r[2]); 
+            return null;
+        }
+        this.en = getQueryString('en');
+        this.cn = getQueryString('cn')
+    },
     mounted() {
         this.$refs.video1.play();
 
