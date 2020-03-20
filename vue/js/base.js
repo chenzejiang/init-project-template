12418ba2a750b74650edf5b3eb8cb998e408cfa3
chenzejiang <>
2020-03-20 16:41:22 +0800

    删除多余文件

--- vue/js/base.js
+++ vue/js/base.js
@@ -1,13 +1,6 @@
 ﻿new Vue({
     el:'#app',
     data: {
-        showBtn: true,
-        message:'hello vue.js.',
-        step: 1, // 步骤
-        tips: '原生听一遍',
-        en: `I'm trying very hard`,
-        cn: '我很努力了',
-        fastAudio: ''
     },
     created() {
         function getQueryString(name) { 
@@ -16,49 +9,9 @@
             if (r != null) return decodeURIComponent(r[2]); 
             return null;
         }
-        this.en = getQueryString('en');
-        this.cn = getQueryString('cn')
     },
     mounted() {
-        
-
-        /* 视频播放完成 */
-        this.$refs.video1.addEventListener("ended", () => {
-            this.$refs.audio1.play();
-        });
-
-        /* 音效1 - 结束 */
-        this.$refs.audio1.addEventListener("ended", () => {
-            this.step = 2;
-            this.tips = '慢速跟读一遍';
-            this.$refs.slowAudio.play()
-        });
-
-        /* 慢音效 播放完成 */
-        this.$refs.slowAudio.addEventListener("ended", () => {
-            this.$refs.audio2.play()
-        });
-
-        /* 音效2 - 结束 */
-        this.$refs.audio2.addEventListener("ended", () => {
-            this.$refs.fastAudio.play()
-            this.step = 3;
-            this.tips = '常速听一遍';
-        });
-
-        /* 快音效 播放完成 */
-        this.$refs.fastAudio.addEventListener("ended", () => {
-            setTimeout(() => {
-                this.step = 1;
-                console.log('结束了重置开始');
-                this.showBtn = true;
-            }, 1000)
-        });
     },
     methods: {
-        playVideo(){
-            this.showBtn = false;
-            this.$refs.video1.play();
-        }
     }
 });
